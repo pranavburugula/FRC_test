b8ea5356257161830377315f71c3848b55ca3bc2
Pranav Burugula <pranav.burugula@gmail.com>
2017-08-06 10:22:24 -0700

    Modified

--- src/org/usfirst/frc/team201800130/robot/DriverStationImpl.java
+++ src/org/usfirst/frc/team201800130/robot/DriverStationImpl.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team201800130.robot;
 
 public class DriverStationImpl {
-
+	FRCRobot robot;
+	public DriverStationImpl(FRCRobot robot) {
+		this.robot = robot;
+	}
 }
--- src/org/usfirst/frc/team201800130/robot/subsystems/Subsystem.java
+++ src/org/usfirst/frc/team201800130/robot/subsystems/Subsystem.java
@@ -1,5 +1,5 @@
 package org.usfirst.frc.team201800130.robot.subsystems;
 
-public interface Attachment {
+public interface Subsystem {
 
 }
